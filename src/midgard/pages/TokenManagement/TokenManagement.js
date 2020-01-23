0c9b8da06153b639192048c7c2018ccaaaf9518b
William Muli <willi.wambu@gmail.com>
2020-01-23 23:11:36 +0300

    Add Popup for creating new application

--- src/midgard/pages/TokenManagement/TokenManagement.js
+++ src/midgard/pages/TokenManagement/TokenManagement.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { colors } from 'colors';
 import { rem } from 'polished';
-import { FjContentSwitcher } from '@buildlyio/freyja-react';
+import { FjButton, FjContentSwitcher } from '@buildlyio/freyja-react';
 import AccessTokens from 'midgard/pages/TokenManagement/AccessTokens/AccessTokens';
 import RefreshTokens from 'midgard/pages/TokenManagement/RefreshTokens/RefreshTokens';
+import Popup from 'reactjs-popup';
+import useWindowDimensions from 'midgard/hooks/useWindowDimensions';
+import ApplicationForm from 'midgard/components/ApplicationForm/ApplicationForm';
+import { createApplication } from 'midgard/redux/applications/application.action';
+import { NotificationContainer } from 'react-notifications';
+import Applications from 'midgard/pages/TokenManagement/applications/Applications';
 
 const TokenManagementWrapper = styled.div`
 	height: 100%;
@@ -14,6 +20,11 @@ const TokenManagementWrapper = styled.div`
 	display: flex;
 	flex: 1;
 	background-color: ${colors.baseLighter};
+	
+	.add_app-button {
+		border: ${rem(1)} solid ${colors.primary};
+    color: ${colors.white};
+	}
 	
 	.tokens {
 		&__container {
@@ -29,6 +40,10 @@ const TokenManagementWrapper = styled.div`
 			align-items: center;
 			margin-bottom: ${rem(30)};
 			padding-right: ${rem(12)};
+			
+			&__name {
+				padding-right: ${rem(12)};
+			}
 		}
 		
 		&__token {
@@ -51,25 +66,49 @@ const TokenManagementWrapper = styled.div`
   }
 `;
 
-const TokenManagement = ({ history, location }) => {
+const TokenManagement = ({ history, location, dispatch }) => {
 	const subNav = [
 		{ label: 'Access tokens', value: 'access-tokens' },
 		{ label: 'Refresh tokens', value: 'refresh-tokens' },
+		{ label: 'Applications', value: 'applications' },
 	];
 	const pathName = '/app/admin/tokens';
 	const childPath = location.pathname.includes('refresh-tokens') ? 'refresh-tokens' : 'access-tokens';
 	const viewState = useState(childPath);
 	const [view, setView] = viewState;
+	const { _, width } = useWindowDimensions();
+	const popoverRef = useRef(null);
+	const POPOVER_WIDTH = 416;
 
 	useEffect(() => {
 		history.push(`${pathName}/${view || location.state}`)
 	}, [view]);
 
+	const handleCreate = (application) => {
+		dispatch(createApplication(application));
+	};
+
 	return (
 		<TokenManagementWrapper>
 			<div className="tokens__container">
 				<div className="tokens__header">
-					<h3 className="tokens__header">Manage authentication tokens</h3>
+					<h3 className="tokens__header__name">Manage authentication tokens</h3>
+					{
+						location.pathname.includes('applications') &&
+						<Popup
+							trigger={<FjButton size="small">Add new Application</FjButton>}
+							offsetX={(width / 2) - POPOVER_WIDTH}
+							offsetY={70}
+							closeOnDocumentClick
+							mouseLeaveDelay={300}
+							mouseEnterDelay={0}
+							contentStyle={{ padding: '0px', border: 'none', width: `{rem(${POPOVER_WIDTH})}` }}
+							arrow={false}
+							ref={popoverRef}
+						>
+							{close => <ApplicationForm action={handleCreate} closeModal={close}/>}
+						</Popup>
+					}
 				</div>
 				<div className="content-switcher__container">
 					<div className="content-switcher">
@@ -82,6 +121,7 @@ const TokenManagement = ({ history, location }) => {
 				<Route path={`${pathName}/access-tokens`} component={AccessTokens}/>
 				<Route path={`${pathName}/refresh-tokens`} component={RefreshTokens}/>
 			</div>
+			<NotificationContainer/>
 		</TokenManagementWrapper>
 	);
 };
